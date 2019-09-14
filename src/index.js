161f0e3fb385aecdf87f8638a6f01e969a77b959
Andrea Mogren <andrea.mogren@comprend.com>
2019-09-14 14:24:23 +0200

    exercise 1.7 wip

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 
 const FeedbackButton = ({onClick, text}) => {
     return (
-        <>
         <button onClick={onClick}>{text}</button>
-        </>
+    )
+}
+
+const DisplayScores = ({text, score}) => {
+    return (
+        <p>{text} {score}</p>
     )
 }
 
@@ -13,6 +17,12 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const score = ((good * 1) + (neutral * 0) + (bad * -1))
+    const total = (good + neutral + bad)
+    const average = score / total 
+    const positiveAverage = (good / total) * 100
+
+    //Set values in functions to avoid displaying NaN?
     return (
         <div>
             <h1>Give feedback</h1>
@@ -20,9 +30,12 @@ const App = () => {
             <FeedbackButton onClick={() => setNeutral(neutral + 1)} text="neutral"/>
             <FeedbackButton onClick={() => setBad(bad + 1)} text="bad"/>
             <h3>Statistics</h3>
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
+            <DisplayScores text="Good:" score={good}/>
+            <DisplayScores text="Neutral:" score={neutral}/>
+            <DisplayScores text="Bad:" score={bad}/>
+            <DisplayScores text="All:" score={total}/>
+            <DisplayScores text="Average:" score={average}/>
+            <DisplayScores text="Positive:" score={positiveAverage}/>
         </div>
     )
 }
