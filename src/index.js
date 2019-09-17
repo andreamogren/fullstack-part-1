2cebf7df299e2f2412d77eaa63ff5c18d72e5cb4
Andrea Mogren <andrea.mogren@comprend.com>
2019-09-17 18:48:35 +0200

    created courseinfo branch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-/* const Header = (props) => {
+const Header = (props) => {
     return (
         <>
             <h1>{props.course}</h1>
@@ -62,81 +62,6 @@ const App = () => {
             <Header course={course.name}/>
             <Content parts={course.parts}/>
             <Total parts={course.parts}/>
-        </div>
-    )
-} */
-/* const Hello = ({name, age}) => {
-    const bornYear = () => new Date().getFullYear() - age
-    return (
-      <div>
-        <p>
-          Hello {name}, you are {age} years old
-        </p>
-        <p>So you were probably born in {bornYear()}</p>
-      </div>
-    )
-  }
-  
-  const App = () => {
-    const name = 'Peter'
-    const age = 10
-  
-    return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />
-      </div>
-    )
-  } 
-  
-  
-ReactDOM.render(<App/>, document.getElementById('root')); */
-
-const Display = ({counter}) => <div>{counter}</div>
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const History = (props) => {
-    if (props.allClicks.length === 0) {
-        return (
-            <div>The app is used by pressing the buttons</div>
-        )
-    }
-    return (
-        <div>Button click history: {props.allClicks.join(' ')}</div>
-    )
-}
-
-
-const App = (props) => {
-    const [counter, setCounter] = useState(0)
-    const setToValue = (value) => setCounter(value)
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-
-    const handleLeftClicks = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
-    const handleRightClicks = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1)
-    }
-
-
-    return (
-        <div>
-            <Display counter={counter}/>
-            <Button text="Plus" onClick={() => setToValue(counter + 1)}/>
-            <Button text="Minus" onClick={() => setToValue(counter - 1)}/>
-            <Button text="Reset" onClick={() => setToValue(0)}/>
-            <div>
-                {left}
-                <Button text="Left" onClick={handleLeftClicks}/>
-                <Button text="Right" onClick={handleRightClicks}/>
-                {right}
-                <History allClicks={allClicks}/>
-            </div>
         </div>
     )
 }
